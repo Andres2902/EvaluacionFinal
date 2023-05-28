7c66a7113751ba11554aa78659d5ccf4fc4c6239
Jdavidrb <131776001+Jdavidrb@users.noreply.github.com>
2023-05-27 22:15:34 -0500

    Update code1.0.py
    Primer versión de menú, con corrección de formato

--- code1.0.py
+++ code1.0.py
@@ -19,3 +19,25 @@ def obtener_resultados(palabra):
     valores_ascii = obtener_ascii(palabra)
     valores_binarios = obtener_binario(palabra)
     return {'ascii': valores_ascii, 'binario': valores_binarios}
+
+
+menu = int(input('Menú\n=====\n1. Carácter\n2. Palabra'))
+
+if menu == 1:
+    caracter = input('Ingresa un carácter: ')
+    palabra = caracter
+    tipo = "carácter"
+elif menu == 2:
+    palabra = input('Ingresa una palabra: ')
+    tipo = "palabra"
+    caracter = palabra  # Utilizar la palabra completa como el valor de caracter
+else:
+    sys.exit()
+
+print('Resultados\n===========')
+
+resultados = obtener_resultados(palabra)
+print('El código ASCII de "{0}" {1} es: {2}'.format(
+    caracter, tipo, resultados['ascii']))
+print('En código binario de "{0}" es: {1}'.format(
+    caracter, resultados['binario']))
